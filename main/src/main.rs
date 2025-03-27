5199042ac4cd279f2b66fe516adf049fb3b10705
fadhil riyanto <me@fadev.org>
2025-03-27 21:21:41 +0700

    add mini redis increment counter
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- main/src/main.rs
+++ main/src/main.rs
@@ -5,13 +5,20 @@ use mini_redis::{Connection, Frame};
 use mini_redis::Command::{self, Get, Set};
 use std::collections::HashMap;
 use bytes::Bytes;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 
 type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
-async fn process( socket: TcpStream, db: Db) {
+async fn process( socket: TcpStream, db: Db, counter: &Arc<Mutex<i32>>) {
     // let mut db: HashMap<String, Vec<u8>> = HashMap::new();
     let mut connection = Connection::new(socket);
+
+    {
+        let counter:Result<MutexGuard<i32>, std::sync::PoisonError<MutexGuard<i32>>>  = counter.lock();
+        if let Ok(mut counter_val) = counter {
+            *counter_val = *counter_val + 1;
+        }
+    }
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
@@ -36,16 +43,7 @@ async fn process( socket: TcpStream, db: Db) {
         connection.write_frame(&response).await.unwrap();
     }
 
-    // if let Some(frame) = connection.read_frame().await.unwrap() {
-    //     println!("got data {:?}", frame);
 
-    //     let response = Frame::Error("uninplemented".to_string());
-    //     connection.write_frame(&response).await.unwrap();
-    // }
-
-    // let mut buf = [65];
-    // socket.write(&buf).await;
-    // println!("data {:?}", connection.read_frame().await.unwrap());
 }
 
 #[tokio::main]
@@ -53,15 +51,18 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     let db: Db = Arc::new(Mutex::new(HashMap::new()));
+    let shared_req_counter: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let shared_req_counter_th: Arc<Mutex<i32>> = shared_req_counter.clone();
+        let shared_req_counter_rt: Arc<Mutex<i32>> = shared_req_counter.clone();
 
         // this is local vars
         let db = db.clone();
         
         tokio::spawn(async move {
-            process(socket, db).await;
+            process(socket, db, &shared_req_counter_th).await;
         });
 
         let handle = tokio::spawn(async {
@@ -70,7 +71,11 @@ async fn main() {
 
         match handle.await {
             Ok(data) => {
-                println!("returned val: {}", data.to_string());
+                {
+                    let shared_req_counter_ref = shared_req_counter_rt.lock().unwrap();
+                    println!("returned val conn {}: {}", shared_req_counter_ref, data.to_string());
+                   
+                }
             },
             Err(err) => {
                 println!("{}", err);
